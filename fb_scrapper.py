b257aaca866cc7baca821c77e094f1a8c51fb046
Daniel Tan <danieltan.singapore@gmail.com>
2016-10-29 18:28:25 +0800

    building

---
+++ fb_scrapper.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 15:00:44 2016
+
+@author: ddan
+"""
+
+import fb_credentials
+
+## Create global variables
+APP_ID = 0
+SECRET_ID = ''
+
+fb_credentials.main(APP_ID,SECRET_ID)
